011d895927b0d375e8494c380b7dfa40245752ff
Anurag22060 <anuragsawal03@gmail.com>
2024-09-21 19:16:45 +0530

    ch3

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const DBconnection = async () => {
-    const MONGO_URI = 'mongodb+srv://anurag:anurag31@cluster0.btpm7pk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    const MONGO_URI = ''
     try{
        await mongoose.connect(MONGO_URI);
        console.log('Database connected successfully');
